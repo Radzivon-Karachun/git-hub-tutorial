14bd13f4a62c2a83b83bd41d37834688075cc4f5
Radzivon-Karachun <karachun1988@gmail.com>
2021-02-22 21:47:30 +0100

    How many Penguins do you want?

---
+++ coursera/Pinguin.py
@@ -0,0 +1,11 @@
+numberPinguin = int(input())
+pinguin0 = '   _~_    \t'
+pinguin1 = '  (o o)   \t'
+pinguin2 = ' /  V  \\ \t'
+pinguin3 = '/(  _  )\\\t'
+pinguin4 = '  ^^ ^^   \t'
+print(pinguin0 * numberPinguin)
+print(pinguin1 * numberPinguin)
+print(pinguin2 * numberPinguin)
+print(pinguin3 * numberPinguin)
+print(pinguin4 * numberPinguin)
