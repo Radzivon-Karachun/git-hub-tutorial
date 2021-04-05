8eac0d72eb87406247d073adb9b9eb76185a2ec9
Radzivon-Karachun <karachun1988@gmail.com>
2021-04-05 20:16:12 +0200

    Syracuse sequence with function decoration

--- Other/Syracuse sequence.py
+++ Other/Syracuse sequence.py
@@ -1,19 +1,32 @@
-import time
-
 n = int(input('Enter the number: '))
-x = 0
-start_time = time.time()
-while True:
-    if n % 2 == 0:
-        n = n // 2
-    else:
-        n = (n * 3) + 1
-    x += 1
-    print(x,':', n)
-    if n == 1:
-        break
-print("--- %s seconds ---" % (time.time()-start_time))
 
 
+def benchmark(func):
+    import time
+
+    def wrapper():
+        start = time.time()
+        func()
+        end = time.time()
+        print('Program execution time: {} seconds'.format(end - start))
+        print("--- %s seconds ---" % (end - start))
+
+    return wrapper
 
 
+@benchmark
+def syracuse_sequence():
+    global n
+    x = 0
+    while True:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = (n * 3) + 1
+        x += 1
+        print(x, ':', n)
+        if n == 1:
+            break
+
+
+syracuse_sequence()
